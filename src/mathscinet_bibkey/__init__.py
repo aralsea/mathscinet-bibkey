e2472ecb1b1de68783d0658f694641c087710cf7
Hayato Arai <hayato4d@gmail.com>
2024-07-08 23:12:48 +0900

    Fix help for file path

--- src/mathscinet_bibkey/__init__.py
+++ src/mathscinet_bibkey/__init__.py
@@ -6,13 +6,11 @@ from mathscinet_bibkey.lib import create_converted_file
 
 
 @click.command()
-@click.argument(
-    "path", type=click.Path(exists=True), help="Path to the target BibTeX file"
-)
-def main(path: click.Path) -> None:
+@click.argument("bibtex_file_path", type=click.Path(exists=True))
+def main(bibtex_file_path: click.Path) -> None:
     """
     This script converts citation keys in a specified BibTeX file from MathSciNet's MRxxxxxxx format to a more readable authorYYYYtitle format.
     e.g. MR0463157 (= Robin Hartshorne, `Algebraic geometry`) -> hartshorne1977algebraic
     """
-    path = Path(path)
+    path = Path(bibtex_file_path)
     create_converted_file(bibtex_file_path=path)
