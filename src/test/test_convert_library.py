5cd02a01c6d6e0b9986b6be53b0f6d405b6f663d
Hayato Arai <hayato4d@gmail.com>
2024-07-11 23:51:31 +0900

    Fix test

--- src/test/test_convert_library.py
+++ src/test/test_convert_library.py
@@ -1,15 +1,24 @@
 from pathlib import Path
 
 import pytest
-from bibtexparser.library import Library
+from bibtexparser.entrypoint import parse_file
+from readable_bibkey.lib import convert_library
 
-TEST_TARGET_PATH_STR = ["../input/hartshorne.bib", "../input/evans_lekili.bib"]
+TEST_TARGET_PATH_STR = ["src/input/hartshorne.bib", "src/input/evans_lekili.bib"]
 
 
 @pytest.mark.parametrize(
     ("path"), [Path(path_str) for path_str in TEST_TARGET_PATH_STR]
 )
 def test_convert_library(path: Path) -> None:
-    library = Library(str(path))
-    expected_library = Library(str(path.parent / f"expected_{path.name}"))
-    assert library.entries_dict == expected_library.entries_dict
+    library = parse_file(path=str(path))
+    convert_library(library=library)
+    expected_library = parse_file(path=str(path.parent / f"expected_{path.name}"))
+    assert [entry.key for entry in library.entries] == [
+        entry.key for entry in expected_library.entries
+    ]
+
+
+if __name__ == "__main__":
+    for path in TEST_TARGET_PATH_STR:
+        test_convert_library(Path(path))
