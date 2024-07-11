72a044f91f56688c4b60850be708712e07ea9a63
Hayato Arai <hayato4d@gmail.com>
2024-07-11 23:14:57 +0900

    Fix encoding

--- src/readable_bibkey/__main__.py
+++ src/readable_bibkey/__main__.py
@@ -1,5 +1,9 @@
+import io
 import sys
 
 import readable_bibkey
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="utf-8")
+sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding="utf-8")
 
 sys.exit(readable_bibkey.main())
